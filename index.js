02491566130436baa761a5f7b8c15c82ec10c762
codekideric <githubnetlify13@gmail.com>
2024-06-03 00:51:23 +0100

    newwupdatenow

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const bookingRoutes = require("./routes/booking.js");
 const userRoutes = require("./routes/user.js");
 
 // Use CORS middleware with custom options
-app.use(cors({
-  origin: 'https://website-79fc5fea.hjm.zca.mybluehost.me'
-}));
+app.use(cors(
+//   {
+//   origin: 'https://website-79fc5fea.hjm.zca.mybluehost.me'
+// }
+));
 
 app.use(express.json());
 app.use(express.static("public"));
