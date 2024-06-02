5357e2724182cced1e235b68aa244b3aec926df0
codekideric <githubnetlify13@gmail.com>
2024-06-02 13:01:30 +0100

    new vercwhy

--- server/index.js
+++ server/index.js
@@ -4,20 +4,25 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth.js")
-const listingRoutes = require("./routes/listing.js")
-const bookingRoutes = require("./routes/booking.js")
-const userRoutes = require("./routes/user.js")
+const authRoutes = require("./routes/auth.js");
+const listingRoutes = require("./routes/listing.js");
+const bookingRoutes = require("./routes/booking.js");
+const userRoutes = require("./routes/user.js");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
 /* ROUTES */
-app.use("/auth", authRoutes)
-app.use("/properties", listingRoutes)
-app.use("/bookings", bookingRoutes)
-app.use("/users", userRoutes)
+app.use("/auth", authRoutes);
+app.use("/properties", listingRoutes);
+app.use("/bookings", bookingRoutes);
+app.use("/users", userRoutes);
+
+/* HELLO ROUTE */
+app.get("/hello", (req, res) => {
+  res.send("Hello");
+});
 
 /* MONGOOSE SETUP */
 const PORT = 3001;
