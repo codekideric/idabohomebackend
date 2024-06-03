ef9b2c99bb538618f909eb8d9c4dc3629a0151bc
codekideric <githubnetlify13@gmail.com>
2024-06-03 11:01:36 +0100

    newwup

--- routes/booking.js
+++ routes/booking.js
@@ -1,70 +1,18 @@
-const router = require("express").Router();
-const nodemailer = require("nodemailer");
-const Booking = require("../models/Booking");
-require('dotenv').config();
+const router = require("express").Router()
 
-// Configure the transporter
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
+const Booking = require("../models/Booking")
 
 /* CREATE BOOKING */
 router.post("/create", async (req, res) => {
   try {
-    const { customerId, customerName, customerPhone, customerEmail, hostId, listingId, startDate, endDate, totalPrice } = req.body;
-
-    // Check for overlapping bookings
-    const existingBooking = await Booking.findOne({
-      listingId,
-      $or: [
-        { startDate: { $lte: endDate, $gte: startDate } },
-        { endDate: { $gte: startDate, $lte: endDate } },
-      ],
-    });
-
-    if (existingBooking && new Date(existingBooking.endDate) >= new Date()) {
-      // If there's an existing booking and it's not yet over, return an error response
-      return res.status(400).json({ 
-        message: "The listing is already booked for the selected dates.",
-        existingBooking: {
-          startDate: existingBooking.startDate,
-          endDate: existingBooking.endDate
-        },
-        alreadyBooked: true
-      });
-    }
-
-    // If there's no overlapping booking, create a new booking
-    const newBooking = new Booking({ customerId, customerName, customerPhone, customerEmail, hostId, listingId, startDate, endDate, totalPrice });
-    await newBooking.save();
-
-    // Send email confirmation to the customer
-    const mailOptions = {
-      from: process.env.EMAIL,
-      to: 'idabohomes2@gmail.com',
-      subject: 'New Booking Confirmation',
-      text: `Booking Details:
-        - Customer Name: ${customerName}
-        - Customer Phone: ${customerPhone}
-        - Customer Email: ${customerEmail}
-        - Start Date: ${startDate}
-        - End Date: ${endDate}
-        - Total Price: ${totalPrice}`,
-    };
-
-    await transporter.sendMail(mailOptions);
-
-    // Send success response
-    res.status(200).json({ alreadyBooked: false });
+    const { customerId, hostId, listingId, startDate, endDate, totalPrice } = req.body
+    const newBooking = new Booking({ customerId, hostId, listingId, startDate, endDate, totalPrice })
+    await newBooking.save()
+    res.status(200).json(newBooking)
   } catch (err) {
-    console.log(err);
-    // If there's any error, send error response
-    res.status(400).json({ message: "Fail to create a new Booking!", error: err.message });
+    console.log(err)
+    res.status(400).json({ message: "Fail to create a new Booking!", error: err.message })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
